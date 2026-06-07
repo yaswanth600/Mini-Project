4808f5bd507fe91eebe91665dda24e5a36384dbd
Yaswanth Jonnada <yeswanthjonnada2004@gmail.com>
2026-06-07 10:30:57 +0530

    edited the 3 rd line

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 import pandas as pd 
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn. model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error,r2_score
